f599a4dc7636d00fc987f795a0ba32aa3be6ed4c
zone2415 <stanislav.kuznetsov.9@mail.ru>
2022-12-08 17:42:17 +0700

    q

--- concert/Views/MainWindow.axaml.cs
+++ concert/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace concert.Views;
 
@@ -7,5 +8,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void OpenMenu(object? sender, RoutedEventArgs e)
+    {
+        MenuItem_One.Open();
     }
 }
